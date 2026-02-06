a2b9efa4bd7ace185a23906bc69b194529c9b21c
NaraTube <naratubejp@gmail.com>
2026-02-07 03:07:28 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,7 @@
 const loginButton = document.getElementById("login")
-const signeIn = document.getElementById("signIn")
 
 
 loginButton.addEventListener("click",
     () => {
          location.href = "login.html";
     });
-
-signeIn.addEventListener("click",
-    () => {
-         location.href = "signIn.html";
-    });
-
